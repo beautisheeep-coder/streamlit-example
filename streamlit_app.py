c4d192c6fb59e7c1b58f1f76eac9e69d3b52e0f5
beautisheeep-coder <beautisheeep@gmail.com>
2026-09-20 15:22:07 +0800

    Change app title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# mSpeeee‘s Web App 🎉
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
